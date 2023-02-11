db713a83dba61967d00b131800deece02eab0365
Gaurav <gjs190201@gmail.com>
2023-02-12 05:23:33 +0530

    integrated 3 more html pages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 from sklearn.preprocessing import StandardScaler
-sc =  StandardScaler()
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,6 +12,21 @@ model = pickle.load(open("Rockerz.pkl", "rb"))
 def home():
     return render_template("index.html")
 
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/crop_recommender")
+def crop_recommender():
+    return render_template("crop_recommender.html")
+
+@app.route("/crop_prices")
+def crop_prices():
+     return render_template("crop_prices.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -24,11 +39,11 @@ def predict():
     ph_soil = request.form["PH_SOIL"]
     rainfall = request.form["RAINFALL"]
     
-    # Use the model to predict the crop recommendation
-    prediction = model.predict(sc.fit_transform([[nitrogen_content, phosphorous_content, potassium_content, temperature, humidity, ph_soil, rainfall]]))
+    # Using the ml model to predict the crop recommendation
+    prediction = model.predict([[nitrogen_content, phosphorous_content, potassium_content, temperature, humidity, ph_soil, rainfall]])
     prediction_text = "The recommended crop is: " + prediction
     
-    return render_template("index.html", prediction_text=prediction_text)
+    return render_template("crop_recommender.html", prediction_text = prediction_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
